bf8e1c090103daf1cdfcdd99e5ce216c373a7534
Anton <vip.valko@gmail.com>
2018-03-27 20:51:48 +0300

    hotfix

--- Tetris_v1.py
+++ Tetris_v1.py
@@ -1,5 +1,6 @@
-import pygame, sys
-from pygame.locals import*
+import pygame
+import sys
+from pygame.locals import *
 import random
 import time
 from collections import Counter
@@ -7,8 +8,8 @@ from collections import Counter
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 GREY = (249, 249, 249)
-RED = (255,22,22)
-GREEN = (0,255,0)
+RED = (255, 22, 22)
+GREEN = (0, 255, 0)
 colors = [WHITE, GREY, RED, GREEN]
 pygame.init()
 x = 200
@@ -16,20 +17,22 @@ y = 400
 display = pygame.display.set_mode((x, y))
 pygame.display.set_caption('Tetris!')
 
-shape_t = [[1,1,1],
-           [0,1,0]]
-shape_p = [[1,1,1,1],
-           [0,0,0,0]]
-shape_b = [[1,1],
-            [1,1]]
-shape_horse = [[1,1,1],
-               [0,0,1]]
-shape_z = [[1,1,0],
-           [0,1,1]]
+shape_t = [[1, 1, 1],
+           [0, 1, 0]]
+shape_p = [[1, 1, 1, 1],
+           [0, 0, 0, 0]]
+shape_b = [[1, 1],
+           [1, 1]]
+shape_horse = [[1, 1, 1],
+               [0, 0, 1]]
+shape_z = [[1, 1, 0],
+           [0, 1, 1]]
+
 
 def random_xset():
-    x = random.choice([x for x in range(20, 121) if x%20 == 0])
+    x = random.choice([x for x in range(20, 121) if x % 20 == 0])
     return x
+
 
 def rotate(shape):
     colums = len(shape[0])
@@ -51,21 +54,24 @@ def rotate(shape):
         new_shape = shape
     return new_shape
 
-def get_cords_shape(x,y, shape):
+
+def get_cords_shape(x, y, shape):
     start_x = x
     pack = []
     for set in shape:
         for number in set:
             if number != 0:
-                pack.append([x,y])
+                pack.append([x, y])
             x += 20
         y += 20
         x = start_x
     return pack
 
+
 def draw_shape(cords):
     for i in cords:
         pygame.draw.rect(display, WHITE, (i[0], i[1], 20, 20))
+
 
 def collision(retire, list_of_cords):
     for l in list_of_cords:
@@ -77,16 +83,17 @@ def collision(retire, list_of_cords):
                 if (l[1] + 20 == z[1] and z[0] == l[0]):
                     return False
 
+
 def collision_sides(list_of_cords, move, retire):
     if move == "up" and len(retire) > 0:
         for i in retire:
             for z in i:
                 for l in list_of_cords:
-                    if (l[0] == z[0] and l[1] == z[1]) or l[0] <= 0 or l[0] >= x-20 or l[1] >= y:
+                    if (l[0] == z[0] and l[1] == z[1]) or l[0] <= 0 or l[0] >= x - 20 or l[1] >= y:
                         return False
     elif move == "up" and len(retire) == 0:
         for l in list_of_cords:
-            if l[0] <= 0 or l[0] >= x-20 or l[1] == y-20:
+            if l[0] <= 0 or l[0] >= x - 20 or l[1] == y - 20:
                 return False
     elif move == "left" and len(retire) > 0:
         for i in retire:
@@ -98,16 +105,17 @@ def collision_sides(list_of_cords, move, retire):
         for i in retire:
             for z in i:
                 for i in list_of_cords:
-                    if i[0] == x-20 or (i[0] + 20 == z[0] and i[1] == z[1]):
+                    if i[0] == x - 20 or (i[0] + 20 == z[0] and i[1] == z[1]):
                         return False
     elif move == "right" and len(retire) == 0:
         for i in list_of_cords:
-            if i[0] == x-20:
+            if i[0] == x - 20:
                 return False
     elif move == "left" and len(retire) == 0:
         for i in list_of_cords:
             if i[0] == 0:
                 return False
+
 
 def burn(retire):
     burn_row = - 247
@@ -119,7 +127,7 @@ def burn(retire):
     if burn_row > 0:
         rm = ([z for i in retire for z in i if z[1] == burn_row])
         for i in rm:
-           for z in retire:
+            for z in retire:
                 if i in z:
                     z.remove(i)
         for i in retire:
@@ -130,7 +138,8 @@ def burn(retire):
                     z[1] += 20
     return retire
 
-shapes = [ shape_p, shape_z, shape_horse, shape_b, shape_t]
+
+shapes = [shape_p, shape_z, shape_horse, shape_b, shape_t]
 object_shape = random.choice(shapes)
 start_y = 0
 start_x = random_xset()
@@ -144,7 +153,7 @@ while True:
     display.fill(BLACK)
     list_of_cords = get_cords_shape(start_x, start_y, object_shape)
     draw_shape(list_of_cords)
-    
+
     for i in list_of_cords:
         if i in top_blocks:
             pygame.quit()
